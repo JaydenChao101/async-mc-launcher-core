62177565ddd2cabcaa2d40a325017fe2b9acf8ef
Jayden Chao <jaydenchao@proton.me>
2025-07-16 21:25:19 +0800

    remove: delete __main__.py

--- launcher_core/__main__.py
+++
@@ -1,9 +0,0 @@
-print("Hello from async-mc-launcher-core!")
-
-# check version
-from .check_version import check_version
-from . import __version__
-import asyncio
-
-asyncio.run(check_version())
-print(f"Current version: {__version__}")
